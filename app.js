da3bfc82c5d16dcf1ee957c8be79127adfdf5a05
Julita Felczuk <80294@student.pb.edu.pl>
2022-11-30 23:35:25 +0100

    Piękny css :)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-class produkt {
-    constructor(id, nazwa, cena, ilosc) {
+class Product {
+    constructor(id, name, price, quantity) {
         this.id = id;
-        this.nazwa = nazwa;
-        this.cena = cena;
-        this.ilosc = ilosc;
-        this.suma = Math.round(this.ilosc * this.cena * 100)/100;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.sum = Math.round(this.quantity * this.price * 100)/100;
         this.deleteButton = document.createElement("button");
         this.deleteButton.innerHTML = "&times";
         this.upButton = document.createElement("button");
@@ -14,230 +14,216 @@ class produkt {
         this.downButton.innerHTML = "&#8681";
 
         this.getSum = function() {
-          return this.suma;
+          return this.sum;
         }
 
     }
-
-
 }
 
-class listaParagon {
+class Receipt {
     constructor() {
-      this.lista = [];
-      this.DeleteRecord = deleteRecord;
+      this.list = [];
+      this.sumReceipt = 0;
     
-this.dodajProdukt = function() {
-    const form =  document.getElementById("myForm");
-    this.lista.push(new produkt(this.lista.length,form.elements['nazwa'].value,form.elements['cena'].value,form.elements['ilosc'].value));
-    this.update();
-    }
+    this.addProduct = function() {
+      const form =  document.getElementById("myForm");
 
-    this.getRazem = function() {
-      var tmp =0;
-      for(var i = 0; i<this.lista.length;i++){
-        tmp = tmp + this.lista[i].getSum;
+      //Validacja danych
+      if(form.elements['name'].value=="" || form.elements['price'].value=="" || form.elements['quantity'].value==""){
+        alert("Pole nie może być puste");
+        return false;
       }
-      return tmp;
+      else if (!isNaN(form.elements['name'].value) || isNaN(form.elements['price'].value) || isNaN(form.elements['quantity'].value)) {
+        alert("Wprowadzono niepoprawny format danych");
+        return false;
+      }
+      else if(form.elements['price'].value <= 0 || form.elements['quantity'].value <= 0){
+        alert('Wprowadź liczbę wiekszą od 0');
+        return false;
+      }
+
+      var obj = new Product(this.list.length,form.elements['name'].value,form.elements['price'].value,form.elements['quantity'].value);
+      obj.sumReceipt = this.getSumReceipt();
+      this.list.push(obj);
+      this.update();
+      }
+
+    this.getSumReceipt = function() {
+      var tmp = 0;
+      for(var i = 0; i<this.list.length;i++){
+        tmp = tmp + this.list[i].sum;
+      }
+      this.sumReceipt = tmp;
+      return this.sumReceipt;
     }
 
-
-function deleteRecord(event) {
-    let index = -1;
-        for (let i = 0; i < this.lista.length; i++) {
-          if (event.target == this.lista[i].deleteButton) {
+    this.deleteRecord = function(event) {
+      let index = -1;
+        for (let i = 0; i < this.list.length; i++) {
+          if (event.target == this.list[i].deleteButton) {
             index = i;
             break;
           }
         }
-        if (confirm("Are you sure you want to delete this record?")) {
+        if (confirm("Czy napewno usunąć tą pozycję?")) {
           console.log(index);
-          this.lista.splice(index, 1);
+          this.list.splice(index, 1);
           this.update();
+        }
+    };
 
-         }
-        };
-
-this.up = function(event) {
-        const table =  document.getElementById("myTable");
-
-        let index = -1;
-        for (let i = 0; i < this.lista.length; i++) {
-          if (event.target == this.lista[i].upButton) {
-            index = i;
-            break;
-          }
-        }      
-        if (index > 0) {
-            let temp = this.lista[index - 1];
-            this.lista[index - 1] = this.lista[index];
-            this.lista[index] = temp;
-            this.update();
-          }
+    this.up = function(event) {
+      const table =  document.getElementById("myTable");
+      let index = -1;
+      for (let i = 0; i < this.list.length; i++) {
+        if (event.target == this.list[i].upButton) {
+          index = i;
+          break;
+        }
+      }      
+      if (index > 0) {
+        let temp = this.list[index - 1];
+        this.list[index - 1] = this.list[index];
+        this.list[index] = temp;
+        this.update();
+      }
     }
 
-this.down = function(event) {
-        const table =  document.getElementById("myTable");
-        let index = -1;
-        for (let i = 0; i < this.lista.length; i++) {
-          if (event.target == this.lista[i].downButton) {
-            index = i;
-            break;
-          }
-        }      
-        if (index < this.lista.length-1) {
-            let temp = this.lista[index + 1];
-            this.lista[index + 1] = this.lista[index];
-            this.lista[index] = temp;
-            this.update();
-          }
+    this.down = function(event) {
+      const table =  document.getElementById("myTable");
+      let index = -1;
+      for (let i = 0; i < this.list.length; i++) {
+        if (event.target == this.list[i].downButton) {
+          index = i;
+          break;
+        }
+      }      
+      if (index < this.list.length-1) {
+        let temp = this.list[index + 1];
+        this.list[index + 1] = this.list[index];
+        this.list[index] = temp;
+        this.update();
+      }
     }
           
- this.update = function() {
+    this.update = function() {
+      const table =  document.getElementById("myTable");
+      table.innerHTML = "";
 
-    const table =  document.getElementById("myTable");
-    table.innerHTML = "";
-
-    var tr = document.createElement("TR");
-        tr.setAttribute("id", 0);
-        document.getElementById("myTable").appendChild(tr);
+      var tr = document.createElement("TR");
+      tr.setAttribute("id", 0);
+      document.getElementById("myTable").appendChild(tr);
     
-        var z = document.createElement("th");
-        var t = document.createTextNode("LP");
-        z.appendChild(t);
-        var z1 = document.createElement("th");
-        var t1 = document.createTextNode("NAZWA");
-        z1.appendChild(t1);
-        var z2 = document.createElement("th");
-        var t2 = document.createTextNode("ILOŚĆ");
-        z2.appendChild(t2);
-        var z3 = document.createElement("th");
-        var t3 = document.createTextNode("CENA");
-        z3.appendChild(t3);
-        var z4 = document.createElement("th");
-        var t4 = document.createTextNode("SUMA");
-        z4.appendChild(t4);
+      var z = document.createElement("th");
+      var t = document.createTextNode("LP");
+      z.appendChild(t);
+      var th1 = document.createElement("th");
+      var text1 = document.createTextNode("NAZWA");
+      th1.appendChild(text1);
+      var z2 = document.createElement("th");
+      var t2 = document.createTextNode("ILOŚĆ");
+      z2.appendChild(t2);
+      var z3 = document.createElement("th");
+      var t3 = document.createTextNode("CENA");
+      z3.appendChild(t3);
+      var z4 = document.createElement("th");
+      var t4 = document.createTextNode("SUMA");
+      z4.appendChild(t4);
 
-        document.getElementById(0).appendChild(z);
-        document.getElementById(0).appendChild(z1);
-        document.getElementById(0).appendChild(z2);
-        document.getElementById(0).appendChild(z3);
-        document.getElementById(0).appendChild(z4);
+      document.getElementById(0).appendChild(z);
+      document.getElementById(0).appendChild(th1);
+      document.getElementById(0).appendChild(z2);
+      document.getElementById(0).appendChild(z3);
+      document.getElementById(0).appendChild(z4);
        
-        if(this.lista.length>0){
-       for(var i = 0; i < this.lista.length; i++){
+      if(this.list.length>0){
+        for(var i = 0; i < this.list.length; i++){
         var j=i+1;
         var tr = document.createElement("TR");
         tr.setAttribute("id", j);
         document.getElementById("myTable").appendChild(tr);
-    
         var z = document.createElement("TD");
         var t = document.createTextNode(j);
         z.appendChild(t);
         document.getElementById(j).appendChild(z);
     
-        var z1 = document.createElement("TD");
-        var t1 = document.createTextNode(this.lista[i].nazwa);
-        z1.appendChild(t1);
-        document.getElementById(j).appendChild(z1);
+        var th1 = document.createElement("TD");
+        var text1 = document.createTextNode(this.list[i].name);
+        th1.appendChild(text1);
+        document.getElementById(j).appendChild(th1);
     
         var z2 = document.createElement("TD");
-        var t2 = document.createTextNode(this.lista[i].ilosc);
+        var t2 = document.createTextNode(this.list[i].quantity);
         z2.appendChild(t2);
         document.getElementById(j).appendChild(z2);
     
         var z3 = document.createElement("TD");
-        var t3 = document.createTextNode(this.lista[i].cena);
+        var t3 = document.createTextNode(this.list[i].price);
         z3.appendChild(t3);
         document.getElementById(j).appendChild(z3);
     
         var z4 = document.createElement("TD");
-        var t4 = document.createTextNode(this.lista[i].suma);
+        var t4 = document.createTextNode(this.list[i].sum);
         z4.appendChild(t4);
         document.getElementById(j).appendChild(z4);
     
         var z5 = document.createElement("TD");
         const btn = document.createElement("button");
-        this.lista[i].deleteButton = btn; 
-        this.lista[i].deleteButton.innerHTML = "&times";
-
-        this.lista[i].deleteButton.onclick = this.DeleteRecord.bind(this);
-        z5.appendChild(this.lista[i].deleteButton);
+        this.list[i].deleteButton = btn; 
+        this.list[i].deleteButton.innerHTML = "&times";
+        this.list[i].deleteButton.onclick = this.deleteRecord.bind(this);
+        z5.appendChild(this.list[i].deleteButton);
         document.getElementById(j).appendChild(z5);
     
         var z7 = document.createElement("TD");
         const btn1 = document.createElement("button");
-
-        this.lista[i].upButton = btn1;
-        this.lista[i].upButton.onclick = this.up.bind(this);        
-        this.lista[i].upButton.innerHTML = "&#8679";
-
-        z7.appendChild(this.lista[i].upButton);
+        this.list[i].upButton = btn1;
+        this.list[i].upButton.onclick = this.up.bind(this);        
+        this.list[i].upButton.innerHTML = "&#8679";
+        z7.appendChild(this.list[i].upButton);
         document.getElementById(j).appendChild(z7);
     
         var z8 = document.createElement("TD");
         const btn2 = document.createElement("button");
-        this.lista[i].downButton = btn2;
-        this.lista[i].downButton.innerHTML = "&#8681";
-        this.lista[i].downButton.onclick = this.down.bind(this);
-        z8.appendChild(this.lista[i].downButton);
+        this.list[i].downButton = btn2;
+        this.list[i].downButton.innerHTML = "&#8681";
+        this.list[i].downButton.onclick = this.down.bind(this);
+        z8.appendChild(this.list[i].downButton);
         document.getElementById(j).appendChild(z8);
-        console.log(j);
     }
 
-    var trw1 = document.createElement("TR");
-        trw1.setAttribute("id", "ddd");
-        document.getElementById("myTable").appendChild(trw1);
-    
-        var sf = document.createElement("th");
-        document.getElementById("ddd").appendChild(sf);
+    var tr1 = document.createElement("TR");
+        tr1.setAttribute("id", "idTr");
+        document.getElementById("myTable").appendChild(tr1);
 
-        var ss = document.createElement("th");
-        document.getElementById("ddd").appendChild(ss);
+    for(var i = 0; i < 3; i++ ){
+      var newTh = document.createElement("th");
+      document.getElementById("idTr").appendChild(newTh);
+    }
+        var newTh = document.createElement("th");
+        var textNode = document.createTextNode("RAZEM");
+        newTh.appendChild(textNode)
+        document.getElementById("idTr").appendChild(newTh);
 
-        var sff = document.createElement("th");
-        document.getElementById("ddd").appendChild(sff);
-
-        var ssff = document.createElement("th");
-        document.getElementById("ddd").appendChild(ssff);
-        var sssff = document.createElement("th");
-        document.getElementById("ddd").appendChild(sssff);
-        var sssffff = document.createElement("th");
-        document.getElementById("ddd").appendChild(sssffff);
-
-
-
-        var z222 = document.createElement("th");
-        var t222 = document.createTextNode("RAZEM");
-        z222.appendChild(t222)
-        document.getElementById("ddd").appendChild(z222);
-
-        var z133 = document.createElement("th");
-        var t133 = document.createTextNode(this.getRazem());
-        z133.appendChild(t133)
-        document.getElementById("ddd").appendChild(z133);
+        var newTh1 = document.createElement("th");
+        var newTextNode = document.createTextNode(this.sumReceipt);
+        this.sumReceipt = this.getSumReceipt();
+        newTh1.appendChild(newTextNode)
+        document.getElementById("idTr").appendChild(newTh1);
     }
     
-
-    localStorage.setItem("Oddanie", JSON.stringify(this.lista));
-
-
-
-}
-}
-
-
-
-}
-
-var paragon = new listaParagon();
-
- function fm() {
-  if(JSON.parse(localStorage.getItem("Oddanie"))!= undefined){
-    const lista = JSON.parse(localStorage.getItem("Oddanie"));
-    paragon.lista = lista;
+    localStorage.setItem("Date", JSON.stringify(this.list));
+   }
   }
-  console.log(paragon.lista);
-  paragon.update();
- }
+}
 
+var receipt = new Receipt();
+ function start() {
+  if(JSON.parse(localStorage.getItem("Date")) != undefined){
+    const list = JSON.parse(localStorage.getItem("Date"));
+    receipt.list = list;
+  }
+  console.log(receipt.list);
+  receipt.sumReceipt = receipt.getSumReceipt();
+  receipt.update();
+ }
